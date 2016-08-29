1256f57ae90f5b895eac4eec57b7242d89e363ae
Rubberkat <roydejager@live.nl>
2016-08-29 12:00:37 +0200

    Added start for block animations

---
+++ src/js/components/blocks.js
@@ -0,0 +1,10 @@
+var $ = require('jquery');
+
+$(function () {
+    $(".block-container").hover(function () {
+        $(this).toggleClass("block-hover");
+        $(".block-container").not(this).toggleClass("no-hover");
+
+    });
+
+});
--- src/js/entry.js
+++ src/js/entry.js
@@ -1,2 +1,3 @@
 var burgerMenu = require('./components/burgermenu');
 var index = require('./components/index');
+var block = require('./components/blocks');
