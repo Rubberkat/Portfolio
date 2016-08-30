026203e5dc353c003a1de36affa07476e75b4d51
Rubberkat <roydejager@live.nl>
2016-08-31 00:20:11 +0200

    added auto scroll to block from item to mobile(working but needs improvement)

--- src/js/components/burgermenu.js
+++ src/js/components/burgermenu.js
@@ -1,8 +1,8 @@
 var $ = require('jquery');
 
-$(function() {
+$(function () {
 
-    $('.burger').click(function() {
+    $('.burger').click(function () {
 
         var $parent = $(this).parent();
 
@@ -14,9 +14,32 @@ $(function() {
         }
 
 
-
         $('.burger-container').removeClass('active');
         $parent.addClass('active');
 
     });
+});
+
+$("#works").click(function() {
+    // var id =  $(this).attr('href');
+    $('.burger-container').removeClass('active');
+    $('html, body').animate({
+        scrollTop: $(" .block.block-3").offset().top-40
+    }, 1000);
+});
+
+$("#contact").click(function() {
+    // var id =  $(this).attr('href');
+    $('.burger-container').removeClass('active');
+    $('html, body').animate({
+        scrollTop: $(" .block.block-2").offset().top-40
+    }, 1000);
+});
+
+$("#about").click(function() {
+    // var id =  $(this).attr('href');
+    $('.burger-container').removeClass('active');
+    $('html, body').animate({
+        scrollTop: $(" .block.block-1").offset().top-40
+    }, 1000);
 });
--- src/js/entry.js
+++ src/js/entry.js
@@ -1,3 +1,2 @@
 var burgerMenu = require('./components/burgermenu');
-var index = require('./components/index');
 var block = require('./components/block');
