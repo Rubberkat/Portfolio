06db395e0bb7547e7972b288ba06e8766fbc53e6
roydejager <roydejager@live.nl>
2016-09-14 13:35:49 +0200

    Fixed page showing

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('nunjucks', function () {
 gulp.task('sass', function () {
     return gulp.src('./src/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('dist'));
 });
 
 
