9de0aff918d7676cc613d11b96ad1b48f0edbf88
roydejager <roydejager@live.nl>
2016-09-27 02:56:49 +0200

    Minor improvements to block transition

--- src/js/components/block.js
+++ src/js/components/block.js
@@ -23,6 +23,32 @@ $(document).ready(function () {
     });
 });
 
+$('.color-changer').click(function() {
+    var colors = ["#442F66", '#68489C', '#9365DB', '#003322', '#00704B', '#00A36D', '#006169',
+        '#008E99', '#00B2BF', '#346327', '#51993C', '#66C24D', '#6E5303', '#A67E05',
+        '#CC9B06', '#6E3C18', '#AB5E25', '#D6762E', '#6E2D27', '#AB473D', '#DE5C50',
+        '#612866', '#A544AD', '#D458DE', '#004166', '#006EAD', '#008DDE'];
+
+    block = $(".block");
+
+
+    block.each(function () {
+        var i = colors[Math.floor(Math.random() * colors.length)];
+
+        $(this).css("background-color", i);
+        var index = colors.indexOf(i);
+        colors.splice(index, 1);
+
+    });
+});
+
+$('.color-changer').hover(function () {
+    $('.color-changer').toggleClass('active');
+});
+
+
+
+
 
 $(".block").hover(function () {
     if ($('.block').not('active')) {
